a1bb7c6af97eb756e76600b844758f19a55550ea
davenportnw <davenportnw@gmail.com>
2020-06-01 20:22:43 -0500

    Add API

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 
 
-//Collecting Elements//
+// //Collecting Elements//
 
-let weatherDashboard = document.getElementById("weatherReport");
+// let weatherDashboard = document.getElementById("weatherReport");
 
-    //buttons
-let austinBtn = document.getElementById("austinBtn");
-let chicagoBtn = document.getElementById("chicagoBtn"); 
-let newyorkBtn = document.getElementById("newyorkBtn");
-let orlandoBtn = document.getElementById("olrandoBtn");
-let sanfranciscoBtn = document.getElementById("sanfranciscoBtn");
-let seatleBtn = document.getElementById("seatleBtn");
-let denverBtn = document.getElementById("denverBtn");
-let alantaBtn = document.getElementById("alantaBtn");
+//     //buttons
+// let austinBtn = document.getElementById("austinBtn");
+// let chicagoBtn = document.getElementById("chicagoBtn"); 
+// let newyorkBtn = document.getElementById("newyorkBtn");
+// let orlandoBtn = document.getElementById("olrandoBtn");
+// let sanfranciscoBtn = document.getElementById("sanfranciscoBtn");
+// let seatleBtn = document.getElementById("seatleBtn");
+// let denverBtn = document.getElementById("denverBtn");
+// let alantaBtn = document.getElementById("alantaBtn");
 
+// let searchCitiesBtn = document.getElementById("searchCities");
 
+// $("#submitCity").on("click",function(event) {
+//     event.preventDefault();
 
-//Implementing Api
-var weatherReports = function(city) { 
-    const queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "12894d5822e7f76bc0b52a517964b8f4";
-    $.ajax({
-        url: queryURL,
-        method:"GET"
-    }).then(function(response){
-        weatherDashboard.innerHTML = response;
-    });
+//     var city = $("#searchCities").val();
 
-};
-
-
-$("#austinBtn").on("click",weatherReports("Austin"));
+//     var queryURL = "https://www.api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=12894d5822e7f76bc0b52a517964b8f4";
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//     }
+// };
